7f35197c140930d684a60b7a25828f2572042099
mszahan <mszahan1@gmail.com>
2023-05-11 17:16:22 +0600

    checked the while and for loop

---
+++ loop.js
@@ -0,0 +1,13 @@
+var i = 0
+
+console.log('Genrated from while loop')
+while (i < 10) {
+    console.log(i)
+    i++
+}
+
+console.log('Generated from for loop')
+
+for (i=0; i<10; i++) {
+    console.log(i)
+}
